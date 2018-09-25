f85da8f4d3ae2f55a1febe89c152c8ea151b8573
Lux <luxi@didichuxing.com>
2018-09-25 14:11:08 +0800

    init project

---
+++ EUILayoutDemo/EUILayoutDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EUILayoutDemo
+//
+//  Created by Lux on 2018/9/25.
+//  Copyright © 2018年 Lux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EUILayoutDemo/EUILayoutDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  EUILayoutDemo
+//
+//  Created by Lux on 2018/9/25.
+//  Copyright © 2018年 Lux. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
