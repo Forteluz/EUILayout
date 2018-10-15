773a129a95ace407af5f3fbae9ee13b354248e7f
Lux <luxi@didichuxing.com>
2018-10-15 11:56:43 +0800

    删除 yoga 的 podfile 设置

--- EUILayoutDemo/Pods/Target Support Files/Pods-EUILayoutDemoTests/Pods-EUILayoutDemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EUILayoutDemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_EUILayoutDemoTests
-@end
--- EUILayoutDemo/Pods/Target Support Files/Pods-EUILayoutDemoUITests/Pods-EUILayoutDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EUILayoutDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_EUILayoutDemoUITests
-@end
